590b17a0a073b5dbc76e949643e73db10472cc86
njalsson <njalsson1337@gmail.com>
2019-07-16 02:07:34 +0000

    it actually works, good shit

--- main.py
+++ main.py
@@ -1,37 +1,56 @@
 from PyQt5 import QtWidgets, QtCore
 import subprocess
 import sys
-
-
+import re
+import math
 class App:
 
 	def __init__(self):
+		# get current brightness 
+		process = subprocess.Popen(['light'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		self.slidervalue, stderr = process.communicate()
+		self.slidervalue = str(self.slidervalue)
+		self.slidervalue = self.slidervalue.replace("\\n'", '')
+		self.slidervalue = self.slidervalue.replace("b'", '')
+		print(str(self.slidervalue))
+		self.slidervalue = int(float(self.slidervalue))+1
 		self.root = QtWidgets.QApplication([])
 		self.mainWidget = QtWidgets.QWidget()
 		self.mainLayout = QtWidgets.QVBoxLayout()
 		self.mainWidget.setLayout(self.mainLayout)
 		self.slider = QtWidgets.QSlider()
+		self.slider.setMinimum(2)
+		self.slider.setMaximum(100)
+		self.slider.setValue(self.slidervalue)
 		self.slider.setOrientation(QtCore.Qt.Horizontal)
 		self.slider.setStyleSheet(self.stylesheet())
+		self.slider.valueChanged[int].connect(self.changeValue)
+		self.mainWidget.setGeometry(100,100,500,100)
+		self.text1 = QtWidgets.QLabel()
+		self.slidervalue = str(self.slidervalue)
+		self.text1.setText('percentage is at {}'.format(self.slidervalue))
+		self.mainLayout.addWidget(self.text1,)
 		self.mainLayout.addWidget(self.slider)
 		self.mainWidget.show()
 		self.root.exec_()
-		process = subprocess.Popen(['light'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		self.slidervalue, stderr = process.communicate()
 	#    .call(['light', '-S', str(x)])
+
+	def changeValue(self, value):
+		print(value)
+		value = str(value)
+		self.slidervalue = value
+		self.text1.setText("percentage is at {}".format(self.slidervalue))
+		subprocess.call(['light', '-S', value])
 
 	def stylesheet(self):
   		return """
 			QSlider::groove:horizontal {
 				background: white;
-				height: 100px;
+				height: 40px;
 			}
 
 			QSlider::sub-page:horizontal {
-				background: qlineargradient(x1: 0, y1: 0,    x2: 0, y2: 1,
-					stop: 0 #66e, stop: 1 #bbf);
-				background: qlineargradient(x1: 0, y1: 0.2, x2: 1, y2: 1,
-					stop: 0 #bbf, stop: 1 #55f);
+				background: #ffd27f;
 				height: 40px;
 			}
 
@@ -51,9 +70,16 @@ class App:
 			}
 			QSlider{
 				margin-top: 25px;
-				margin-left: 25px;
-				height: 200px;
-				width: 500px;
+				margin-left: 40px;
+				margin-right: 40px;
+				height: 100px;
+				width: 550px;
+			}
+			.QLabel{
+				background-color: red;
+				margin-left: 100px;
+				margin-top: 100px;
+				font-size: 4em;
 			}
 		"""
 app = App()
